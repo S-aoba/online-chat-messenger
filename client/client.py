2d641c8cbd4cee979a4e3f4990a4ecaa7f2c7c86
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-01-02 13:33:19 +0900

    #1_create_socket

--- client/client.py
+++ client/client.py
@@ -1,3 +1,16 @@
+import socket
+
+class Client:
+  def __init__(self) -> None:
+    self.server_address = '0.0.0.0'
+    self.tcp_server_port = 9001
+    self.udp_server_port = 9002
+
+    self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    self.udp_sock.bind((self.server_address, self.udp_server_port))
+
 def main():
   return
 
--- server/server.py
+++ server/server.py
@@ -1,3 +1,17 @@
+import socket
+
+class Server:
+  def __init__(self) -> None:
+    self.server_address = '0.0.0.0'
+    self.tcp_server_port = 9001
+    self.udp_server_port = 9002
+
+    self.tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    self.udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    self.tcp_sock.bind((self.server_address, self.tcp_server_port))
+    self.udp_sock.bind((self.server_address, self.udp_server_port))
+
 def main():
   return
 
